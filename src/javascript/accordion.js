1ca0b806a90fd0fe26add475c90e2b31527a29cc
Aarón <aaron.sn.1988@gmail.com>
2018-09-25 17:27:45 +0200

    Add transition to accordion

--- src/javascript/accordion.js
+++ src/javascript/accordion.js
@@ -2,14 +2,16 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {        
-        this.classList.toggle("active");
-  
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-} 
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    var icon = this.firstElementChild;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+      icon.className = 'fas fa-plus';
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      icon.className = 'fas fa-minus';
+    }
+  });
+}
