715ca1075ef93a4c80e9d4d6b2eaa9146941b6c6
Aarón <aaron.sn.1988@gmail.com>
2018-09-26 16:41:01 +0200

    Add styles and logic for modal windows

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -39,6 +39,7 @@ const settings = {
         'src/skeleton/dropdown.scss',
         'src/skeleton/accordion.scss',
         'src/skeleton/slideShow.scss',
+        'src/skeleton/modal.scss',
         'src/skeleton/element-colors.scss', //? keep as last element
 
         // // Flexbox-Grid:
