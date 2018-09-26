063ab3bbe5cd8746290a120caef9017ac599e471
Aarón <aaron.sn.1988@gmail.com>
2018-09-26 13:01:20 +0200

    Update css and js

--- javascript/skeleton-styled.js
+++ javascript/skeleton-styled.js
@@ -35,3 +35,24 @@ window.onclick = function(event) {
     }
   }
 } 
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+    showSlides(slideIndex[no] = n, no);    
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
+} 
