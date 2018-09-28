a72dda82a738683e0ab1a669ba066a73441e9d5e
Aarón <aaron.sn.1988@gmail.com>
2018-09-28 13:53:20 +0200

    Remove predeploy logic.

--- predeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$TRAVIS_TAG" ]]; then
-    echo "Skiping predeploy process, TRAVIS_TAG is not defined"
-else
-    npm config set git-tag-version false
-    npm version ${TRAVIS_TAG}
-    npm version   
-fi
