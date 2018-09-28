06a206804fa907398bc408b7bfe8d512962f7ed6
Aarón <aaron.sn.1988@gmail.com>
2018-09-28 10:56:27 +0200

    Add predeploy to travis pipeline

---
+++ predeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [[ -z "${TRAVIS_TAG}" ]]; then
+    echo "Skiping predeploy process, TRAVIS_TAG is not defined"
+else
+    npm config set git-tag-version false
+    npm version ${TRAVIS_TAG}
+    npm version   
+fi
