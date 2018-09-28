7bb79700e1fc2f3d6bbd9c892f3ca3f33ddca69a
Aarón <aaron.sn.1988@gmail.com>
2018-09-28 12:54:51 +0200

    Update css and javascript on push master

---
+++ postScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    git add css/*
+    git add javascript/*
+    git commit -amend -m "Update css and javascript content"
+    git push --force 44r0n/skeleton-styled master
+else
+    echo 'Not in master branch. Skiping push back.'
